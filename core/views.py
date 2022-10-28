0a95a99eb201860ec01599649c2b6fe7c6515311
oleg.gusev <gusevoleg@gmail.com>
2022-10-28 12:08:16 +0300

    change library

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 import json
+import math
 import re
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 
 # Create your views here.
 from django.utils.decorators import method_decorator
@@ -10,23 +11,27 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from dostoevsky.tokenization import RegexTokenizer
 from dostoevsky.models import FastTextSocialNetworkModel
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import torch
+
+tokenizer_sb = GPT2Tokenizer.from_pretrained("sberbank-ai/rugpt3large_based_on_gpt2")
+gpt3_large_sb = GPT2LMHeadModel.from_pretrained("sberbank-ai/rugpt3large_based_on_gpt2")
+
 
 tokenizer = RegexTokenizer()
 model = FastTextSocialNetworkModel(tokenizer=tokenizer)
 
 min_negative = 0.5
 min_positive = 0.5
-min_neutral = 0.8
+min_neutral = 0.6
 
 
-def get_rate(result):
+def get_rate(result, texts):
     negative = result['negative']
     positive = result['positive']
     neutral = result['neutral']
-    # st = ' n: ' + str(negative) + ' p: ' + str(positive) + ' neu: ' + str(neutral) + ' n/neu: ' + str(negative/neutral)+ ' p/neu: ' + str(positive/neutral)
     if neutral >= min_neutral:
         return "neutral"
-    # if negative > min_negative or negative/neutral > min_negative or positive > min_positive or positive/neutral > min_positive:
     if negative > min_negative or positive > min_positive:
         if negative > positive:
             return "neutral"
@@ -36,6 +41,28 @@ def get_rate(result):
         return "negative"
     if positive > negative and positive > neutral:
         return "positive"
+
+    sentence_positive = 'довольна:' + texts
+    sentence_negative = 'недовольна:' + texts
+
+    list_sent = [sentence_positive, sentence_negative]
+    ppl_values = []
+    try:
+        for sentence in list_sent:
+            encodings = tokenizer_sb(sentence, return_tensors='pt')
+
+            input_ids = encodings.input_ids
+            with torch.no_grad():
+                outputs = gpt3_large_sb(input_ids=input_ids, labels=input_ids)
+            loss = outputs.loss
+            ppl = math.exp(loss.item() * input_ids.size(1))
+            ppl_values.append(ppl)
+        if ppl_values[0] > ppl_values[1]:
+            return 'negative'
+        elif ppl_values[0] < ppl_values[1]:
+            return 'positive'
+    except Exception:
+        pass
     return "neutral"
 
 
@@ -66,5 +93,5 @@ class PostDostoevsky(View):
         dost_results = model.predict(texts, k=5)
         res = []
         for i in range(len(ids)):
-            res.append({'id': ids[i], 'rate': get_rate(dost_results[i])})
+            res.append({'id': ids[i], 'rate': get_rate(dost_results[i], texts[i])})
         return HttpResponse(json.dumps(res), content_type='application/json')
